2bf2977e1524b22ead2f586fc31e57f06b468463
abhishekkulka10 <68392339+abhishekkulka10@users.noreply.github.com>
2026-09-13 14:29:47 +0000

    Deploying to gh-pages from @ abhishekkulka10/happy-miles@ed53d7d9537015b8f8807d1d8ab37d12272c91b8 🚀

---
+++ dev/config.js
@@ -0,0 +1,34 @@
+// config.js
+const COMPANY_NAME    = 'Happy Miles';
+const AUTO_REFRESH_MS = 10800000;
+const API_KEY         = 'AIzaSyBDLyEYzxIbwIfpiZj9745vBhbE0_lRYEk';
+const SCRIPT_URL      = 'YOUR_DEPLOYED_APPS_SCRIPT_WEB_APP_URL';
+const DRIVE_FOLDER_ID = 'YOUR_GOOGLE_DRIVE_FOLDER_ID';
+const SHEET_NAME      = '';
+const DATA_RANGE      = '';
+
+const USER_CONFIG = {
+
+    'admin': {
+        password: 'abhishek',
+        spreadsheetId: '1GW7reZMUtYDoc-26DeIBs4bOSM31k8U0JfKidozbkOs'
+    },
+    'owner': {
+        password: 'gzhappy',
+        spreadsheetId: '1GW7reZMUtYDoc-26DeIBs4bOSM31k8U0JfKidozbkOs'
+    },
+    '7944': {
+        password: '7944',
+        spreadsheetId: '1x0BnUziYQemws3Wd0vzsjGxLBoVeLX02fQzdJ6CEuLg'
+    },
+    '8403': {
+        password: '8403',
+        spreadsheetId: '1s6ArloJX8LfzUaNXQbq7WMI0d--N2P93yuQOvtGx8qE'
+    }
+};
+
+const MAINTENANCE_THRESHOLDS = {
+    oilChange: 5000,
+    tireRotation: 10000,
+    brakeInspection: 15000
+};
